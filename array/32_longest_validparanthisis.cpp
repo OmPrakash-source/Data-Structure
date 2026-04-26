bfc695bfcdd4a620959f8db014a64003d498dd6e
OmPrakash-source <182613754+OmPrakash-source@users.noreply.github.com>
2026-04-26 20:41:58 +0530

    feat: add array/32_longest_validparanthisis.cpp

---
+++ array/32_longest_validparanthisis.cpp
@@ -0,0 +1,28 @@
+#include<iostream> 
+using namespace std;
+int longestparn(string s){
+    int left = 0, right = 0, maxlen = 0;
+    for(char ch : s){
+        if(ch == '(') left++;
+        else right++;
+        // agar 1 open or 1 close bracket mille to
+        if(left == right) maxlen = max(maxlen, 2*right);//  valid hai 2 mille iss liye *2
+
+        if(right > left) left = right = 0; // ham right se check kar rahe hai ye "(" extra backet handel karege
+    }
+    left = right = 0;
+    for(char ch : s){
+        if(ch == ')') right++;
+        else left++;
+        // agar 1 open or 1 close bracket mille to
+        if(left == right) maxlen = max(maxlen, 2*left);//  valid hai 2 mille iss liye *2
+
+        if(right < left) left = right = 0; // ham left se check kar rahe hai ye ")" extra backet handel karege
+    }
+    return maxlen;
+}
+
+int main(){
+    string s = "()()((())";
+    cout<<longestparn(s)<<endl;
+}
