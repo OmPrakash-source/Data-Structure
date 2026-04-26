eb63140b6a5bfabbbd47fd237ecb762cb17452d1
OmPrakash-source <182613754+OmPrakash-source@users.noreply.github.com>
2026-04-26 06:28:34 +0530

    Inbuild hash fuction's

--- Hashing/STL.cpp
+++ Hashing/STL.cpp
@@ -3,32 +3,35 @@
 #include<map>
 using namespace std;
 int main(){
+    map<char,int>m;
+    int i=0;
+    cout<<m.count(i); //
     // for create a unordered map
-    // unordered_map<string,float>m;
-    // //assining
-    // m["India"] = 150.66;
-    // m["China"] = 150.43;
-    // m["US"] = 50.5;
-    // m["UK"] = 20.5;
-    // m["Napal"] = 10.5;
-    // //print single value
-    // cout<<m["India"]<<endl;
-    // //full print
-    // for (pair<string,float>country:m){
-    //     cout<<country.first<<", "<<country.second<<endl;
-    // }
-    // //rehase
+    unordered_map<string,float>m;
+    //assining
+    m["India"] = 150.66;
+    m["China"] = 150.43;
+    m["US"] = 50.5;
+    m["UK"] = 20.5;
+    m["Napal"] = 10.5;
+    //print single value
+    cout<<m["India"]<<endl;
+    //full print
+    for (pair<string,float>country:m){
+        cout<<country.first<<", "<<country.second<<endl;
+    }
+    //rehase
     // m.rehash(1);
 
-    // cout<<m.size()<<endl;
+    cout<<m.size()<<endl;
 
-    // //erase
+    //erase
     // m.erase("US");
 
-    // for (pair<string,float>country:m){
-    //     cout<<country.first<<", "<<country.second<<endl;
-    // }
-    // //exist of not
+    for (pair<string,float>country:m){
+        cout<<country.first<<", "<<country.second<<endl;
+    }
+    //exist of not
     // if(m.count("US")){
     //     cout<<"US is exist\n";
     // }else{
