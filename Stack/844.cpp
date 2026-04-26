128f9f9d61b7989145888799f04783a275a98a94
OmPrakash-source <182613754+OmPrakash-source@users.noreply.github.com>
2026-04-26 20:41:25 +0530

    feat: add Stack/844.cpp

---
+++ Stack/844.cpp
@@ -0,0 +1,45 @@
+// if # apare in string then delete one string element which have before #
+
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+void reverse(string &result){
+    int st = 0;
+    int ed = result.size() - 1;
+    while(st < ed){
+        char temp = result[st];
+        result[st] = result[ed];
+        result[ed] = temp;
+        st++;
+        ed--;
+    }
+}
+
+string halper(string str){
+    stack<char>st;
+    for( char ch : str){
+        if(ch == '#'){
+            if(!st.empty()) st.pop();
+        }else{
+            st.push(ch);
+        }
+    }
+    string result;
+    while(!st.empty()){
+        result += st.top(); // yaha result me ulti string aai hai to reverse karna hai
+        st.pop();
+    }
+    reverse(result);
+    return result;
+}
+bool compear_string(string s, string t){
+    return halper(s) == halper(t);
+}
+
+int main(){
+    string s = "ab#c";
+    string t = "ad#c";
+    cout<< compear_string(s, t);
+    return 0;
+}
