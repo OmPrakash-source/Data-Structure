54257d991e76ecf6875a2d24ffb4d7f7412a9441
OmPrakash-source <182613754+OmPrakash-source@users.noreply.github.com>
2026-04-26 20:40:44 +0530

    feat: add DP/temp.cpp

---
+++ DP/temp.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ int fiboBymemo(int n){
+    if(n == 0 || n == 1) return n;
+    vector<int>dp(n+1, -1);
+    if(dp[n] != -1){
+        return dp[n];
+    }
+    dp[n] = fiboBymemo(n-1) + fiboBymemo(n-2);
+    return dp[n];
+ }
+
+ int fiboByteb(int n){
+    vector<int>DP(n+1, 0);
+    DP[0] = 0;
+    DP[1] = 1;
+    for(int i=2; i<=n; i++){
+        DP[i] = DP[i-1] + DP[i-2];
+    }
+    return DP[n];
+ }
+int main(){
+    int n;
+    cin>>n;
+    cout<<fiboByteb(n);
+
+}
